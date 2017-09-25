544e00e70e178cfce081bbdb33278387d5a49b82
mjx1993 <464471063@qq.com>
2017-09-25 17:39:14 +0800

    修改ucpaas函数

--- app/admin/controller/UcpaasController.php
+++ app/admin/controller/UcpaasController.php
@@ -7,20 +7,28 @@ use ucpaas\Ucpaas;
 
 class UcpaasController extends AdminBaseController
 {
-	public function send() {
-		$mobile = '13726032667';
-		$veryify_code = '1e38';
-		$config = config('YUN_ZHI_XUN');
-		$option = [
-        	'accountsid' => $config['YUN_ACCOUNT_SID'],     	// your sid
-        	'token' => $config['YUN_ACCOUNT_TOKEN']        // your token
-    	];
-    	$ucpass = new Ucpaas($option);
-    	$status = $ucpass->templateSMS($config['YUN_APPID'], $mobile, $config['YUN_TEMPLATE_ID'], $veryify_code);
-    	$arr = json_decode($status, true);
-    	if ($arr['resp']['respCode'] == 000000)
-			echo "短信发送成功！";
-    	else
-			echo "短信发送失败！";
+
+	public function test() {
+		$param = $this->request->param();
+		$mobile = $param['mobile'];
+		$code = $param['code'];
+		// dump($mobile.'-'.$code);
+		$rc = send_yun_code($mobile, $code);
+		if ($rc) 
+			echo '短信发送成功！';
+	    else 
+	    	echo '短信发送失败！';
+	}
+
+	public function testzodiac () {
+		$param = $this->request->param();
+		$month = $param['month'];
+		$day = $param['day'];
+		$name = get_zodiac_sign($month, $day);
+		dump($name);
+	}
+
+	public function testqrcode() {
+		$re = qrcode('http://www.baidu.com', 4);
 	}
 }
